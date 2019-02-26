15f3ddf61efb73ec4eae1d0b6d2534a1e3421bf6
Ryan Stipe <ryanstipe@gmail.com>
2019-02-26 00:49:32 -0700

    TetrisClimber

---
+++ Tetris Climber/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    //Movement
+    public float speed;
+    public float jump;
+    private float MoveVelocity;
+
+    //Grounded vars
+    bool grounded = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("escape"))
+        {
+            SceneManager.LoadScene("Scene2");
+        }
+
+        //jumping
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (grounded)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+            }
+        }
+        
+        MoveVelocity = 0;
+
+        //Left Right Movement
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveVelocity = speed;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+
+    }
+    //check if grounded
+
+    void OnTriggerEnter2D()
+    {
+        grounded = true;
+    }
+    void OnTriggerExit2D()
+    {
+        grounded = false;
+    }
+
+  void OnCollisionEnter2D(Collision2D collision)
+    {
+            if (collision.gameObject.tag == "Door")
+        {
+            if(SceneManager.GetActiveScene().name == "wat")
+            {
+                SceneManager.LoadScene("Scene2");
+            }
+            else
+            {
+                SceneManager.LoadScene("wat");
+            }
+            
+        }
+    }
+}
---
+++ Tetris Climber/Assets/Scripts/Tetromino.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tetromino : MonoBehaviour{
+
+    public float speed;
+    private float MoveVelocity;
+    public float rotationspeed;
+    private float rotation;
+    public GameObject[] groups;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveVelocity = 0;
+        //Left Right Movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveVelocity = speed;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnTriggerEnter2D();
+        }
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+        //Rotation
+        rotation = 0;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.E))
+        {
+            rotation = rotationspeed;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.Q))
+        {
+            rotation = -rotationspeed;
+        }
+        GetComponent<Rigidbody2D>().AddTorque(rotation , ForceMode2D.Impulse);
+    }
+    void OnTriggerEnter2D()
+    {
+        enabled = false;
+        gameObject.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        
+    }
+}
